253b0804668f078a433d92e80a8e2c41a6bc3733
Kristin868 <101180814+Kristin868@users.noreply.github.com>
2022-03-10 15:40:56 +0000

    Kristin

--- bin/4.dart
+++ bin/4.dart
@@ -6,4 +6,22 @@ void main() {
 
   // verkefni.4
   // setja inn tölu td. 34893, og prenta út fjölda tölustafa í þessari tölu
+
+  import 'dart:convert';
+  import 'dart:io';
+  import 'dart:math';
+
+  void main() {
+
+    // verkefni.4
+    // nota loop
+    // setja inn tölu td. 34893, og prenta út fjölda tölustafa í þessari tölu
+
+    print('settu inn háa tölu sem inniheldur marga tölustafi: ');
+
+    answer = stdin.readLineSync();
+
+    print('þetta gera: ' + answer.length.toString() + ' tölur');
+  }
+
 }
--- bin/Test33.dart
+++ bin/Test33.dart
@@ -1,8 +1,19 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 
-void main() {
 
-  print('hello');
+void main(List<String>arguments) {
+
+  // verkefni.4
+  // nota loop
+  // setja inn tölu td. 34893, og prenta út fjölda tölustafa í þessari tölu
+
+  String tala;
+
+  print('settu inn háa tölu sem inniheldur marga tölustafi: ');
+
+  tala = stdin.readLineSync();
+
+  print('þetta gera: ' + tala.length.toString() + ' tölur');
 }
+
