8a0de00165480d8149bec882caa1fe259d8cf748
Kristin868 <101180814+Kristin868@users.noreply.github.com>
2022-03-10 15:43:42 +0000

    Update 4.dart

--- bin/4.dart
+++ bin/4.dart
@@ -1,11 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
-
-void main() {
-
-  // verkefni.4
-  // setja inn tölu td. 34893, og prenta út fjölda tölustafa í þessari tölu
 
   import 'dart:convert';
   import 'dart:io';
