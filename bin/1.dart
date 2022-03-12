cba12e911ca5e9a1282110dd0abf7a2f6e4a7588
Kristin868 <101180814+Kristin868@users.noreply.github.com>
2022-03-12 13:53:04 +0000

    Update 1.dart

--- bin/1.dart
+++ bin/1.dart
@@ -2,14 +2,16 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+
 void main() {
 
   //verkefni.1
-  //prenta tölur 1-10 og heildar summu þeirra
-  int x = 5;
+  // setja inn tölur 1-10, prenta svo út summu þeirra = 55
 
+  int sum = 0;
   for (int i = 1; i <= 10; i++) {
-    print(i * x);
+    print(i);
+    sum += i;
+    print(sum);
   }
 }
-
