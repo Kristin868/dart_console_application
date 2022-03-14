23bdde58ce5752fbfdd383e6d7a53909a361629f
Kristin868 <101180814+Kristin868@users.noreply.github.com>
2022-03-14 09:36:33 +0000

    Update 4.dart

--- bin/4.dart
+++ bin/4.dart
@@ -1,19 +1,25 @@
 
-  import 'dart:convert';
-  import 'dart:io';
-  import 'dart:math';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
 
-  void main() {
+int main() {
 
-    // verkefni.4
-    // nota loop
-    // setja inn tölu td. 34893, og prenta út fjölda tölustafa í þessari tölu
+  // verkefni.4
+  // nota loop
+  // setja inn tölu td. 34893, og prenta út fjölda tölustafa í þessari tölu
 
-    print('settu inn háa tölu sem inniheldur marga tölustafi: ');
+  print('settu inn háa tölu sem inniheldur marga tölustafi, en byrjaðu á tölu nr.1 ');
 
-    answer = stdin.readLineSync();
+  bool active = true;
 
-    print('þetta gera: ' + answer.length.toString() + ' tölur');
-  }
+  String input = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
 
-}
+  { print('settu nú tölu nr 2, og svo koll af kolli. þegar þú ert búin, skrifaði þá stop :)');}
+
+  while(active) {input =  stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+
+  if (input == 'stop') {active = false;}
+
+  else {print('skrifaðu næstu tölu: ');
+  }}}
