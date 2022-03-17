5d047a13063971b647d2c5287bc7b88e21d42214
Kristin868 <101180814+Kristin868@users.noreply.github.com>
2022-03-17 11:17:49 +0000

    Create Assignment4.2.dart

---
+++ bin/Assignment4.2.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+
+  //prenta út tölur sem eru stærri en og jafnt og 20
+  //prenta út tölur sem eru minni eða jafnt og 80
+  // semsagt 20-80
+  //prenta út fjölda talna sem uppfylltu skilyrðin
+
+  List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+
+  String concat = '';
+  int counter = calculate_sum.length;
+  int sum = 0;
+  for(int i = 0; i < counter; i++); {
+    sum += calculate_sum[i];
+    print(sum);
+  }
+
+
